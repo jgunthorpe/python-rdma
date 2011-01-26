51160d27b88e0b489bd7be184a87651594fc6542
Jason Gunthorpe <jgunthorpe@obsidianresearch.com>
2011-01-26 16:55:20 -0700

    Move test to tests per python convention
    Add run-tests.py. Call with no args to run everything, call with
    tests.foo to run one thing.

---
+++ run-tests.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import unittest,os,os.path,fnmatch
+import tests
+
+def testAll():
+    """Return a test suite for everything in the test/ directory.
+    Replace me with discover for Python 2.7"""
+    return suite;
+
+module = __import__("tests");
+fns = [os.path.splitext(I)[0] for I in
+         fnmatch.filter(os.listdir(module.__path__[0]),"*.py")];
+fns.remove("__init__");
+for I in fns:
+    __import__("tests." + I);
+suite = unittest.TestSuite(unittest.defaultTestLoader.loadTestsFromNames(fns,module));
+
+if __name__ == "__main__":                   
+    unittest.main(defaultTest="testAll");
