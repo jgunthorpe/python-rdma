5a2c1523487097252de8dda259df2694b5dfd6a6
Jason Gunthorpe <jgunthorpe@obsidianresearch.com>
2011-07-07 17:15:58 -0600

    Move more code out of ibtool and into libibtool
    Mainly so we can re-use the command line parser framework elsewhere,
    but also should speed up loading a tiny bit.

--- libibtool/__init__.py
+++ libibtool/__init__.py
@@ -1,5 +1,2 @@
 # Copyright 2011 Obsidian Research Corp. GPLv2, see COPYING.
-
-class CmdError(Exception):
-    """Thrown for any command line or command operation failures."""
-    pass;
+from .tools import CmdError;
--- libibtool/tools.py
+++ libibtool/tools.py
@@ -5,6 +5,10 @@ import sys;
 import optparse;
 import inspect;
 
+class CmdError(Exception):
+    """Thrown for any command line or command operation failures."""
+    pass;
+
 class MyHelpFormatter(optparse.IndentedHelpFormatter):
     def format_usage(self, usage):
         return usage + "\n";
@@ -44,3 +48,76 @@ class MyOptParse(optparse.OptionParser):
     def format_help(self, formatter=None):
         """Defer computing the help text until it is actually asked for."""
         return optparse.OptionParser.format_help(self,formatter);
+
+default_module = __name__.rpartition('.')[0];
+
+def get_cmd_func(name):
+    # Fetch the commands dict from the top level
+    commands = sys.modules['__main__'].commands;
+
+    loc = commands[name];
+    module = "." + name;
+    func = "cmd_%s"%(name);
+    shown = True;
+    if loc is not None:
+        if len(loc) >= 3:
+            shown = loc[2];
+        if len(loc) >= 2:
+            func = loc[1];
+        if len(loc) >= 1:
+            module = loc[0];
+
+    if module[0] == '.':
+        module = default_module + module;
+
+    rmodule = sys.modules.get(module);
+    if rmodule is None:
+        __import__(module);
+        rmodule = sys.modules[module];
+    return getattr(rmodule,func),shown;
+
+def cmd_help(argv,o):
+    """Display the help text
+       Usage: %prog"""
+    # Fetch the commands dict from the top level
+    commands = sys.modules['__main__'].commands;
+
+    (args,values) = o.parse_args(argv);
+
+    if len(argv) == 0:
+        # Abuse the optparse help formatter to format the help text for
+        # our commands
+        class Formatter(MyHelpFormatter):
+            def format_option(self, option):
+                if option.action == "help":
+                    self.option_strings[option] = "help";
+                else:
+                    self.option_strings[option] = self.option_strings[option][3:];
+                return MyHelpFormatter.format_option(self,option);
+
+        o = MyOptParse(cmd_help);
+        for k in sorted(commands.iterkeys()):
+            if k == "help":
+                continue
+            func,shown = get_cmd_func(k);
+            if not shown:
+                continue;
+            doc = inspect.getdoc(func);
+            doc = [i for i in doc.split("\n") if len(i) != 0];
+            o.add_option("--x" + k,action="store_true",help=doc[0]);
+
+        prog = os.path.basename(sys.argv[0]);
+        print "%s - %s\n"%(prog,o.top_mod.banner)
+        print "Usage: %s command [args]"%(prog)
+        print
+        print o.format_option_help(Formatter());
+        print "%s help [command] shows detailed help for each command"%(prog)
+        return True;
+
+    if len(argv) == 1 and commands.has_key(argv[0]):
+        func,shown = get_cmd_func(argv[0]);
+        o = MyOptParse(func);
+        func(["--help"],o);
+    else:
+        print "No help text for %s"%(argv);
+    return True;
